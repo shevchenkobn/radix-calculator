e341a41138c37e1617315e0452d6b296f5a1a3b6
shevchenkobn <bohdan.shevchenko1@nure.ua>
2017-11-05 12:51:58 +0200

    worked out visualization

--- app/app.js
+++ app/app.js
@@ -1,4 +1,289 @@
-angular.module('radix-converter', ["ui.bootstrap"])
+function RadixConverter() {
+  var convertTokens = {};
+  function toDecimal(token) {
+    if (!convertTokens[token]) {
+      var num = parseInt(token);
+      if (num >= 0 && num < 10) {
+        convertTokens[token] = num;
+      } else {
+        token += '';
+        var code = token.charCodeAt(0);
+        if (convertTokens[token] < 65 ||
+          convertTokens[token] > 90) {
+          throw new TypeError('Not an uppercase letter nor number');
+        }
+        convertTokens[token] = code - 55;
+      }
+    }
+    return convertTokens[token];
+  }
+  function toArbitrary(num) {
+    if (typeof num !== 'number' || num < 0 || num > 36) {
+      throw new TypeError('Number must be between 2 and 36');
+    }
+    num = parseInt(num);
+    if (!convertTokens[num]) {
+      if (num < 10) {
+        convertTokens[num] = num;
+      } else {
+        convertTokens[num] = String.fromCharCode(55 + num);
+      }
+    }
+    return convertTokens[num];
+  }
+  var alphaNumRegex = /^[A-Z0-9]+$/;
+  function isAnyRadixCypher(str) {
+    return alphaNumRegex.test(str);
+  }
+  
+  var fractionLimit = -1;
+  var crop = false;
+  var delimiter = '.';
+  function convert(num, inputRadix, outputRadix)
+  {
+    inputRadix = parseInt(inputRadix);
+    outputRadix = parseInt(outputRadix);
+    if (isNaN(outputRadix) || isNaN(inputRadix) || inputRadix < 2
+      || inputRadix > 36 || outputRadix < 2 || outputRadix > 36) {
+      throw new TypeError('Radix must been between 2 and 36');
+    } else if (inputRadix === outputRadix) {
+      throw new TypeError('Radixes are equal');
+    }
+    
+    var result = 0;
+    if (outputRadix === 10) {
+      num = (num + '').toUpperCase();
+      var maxCipher = toArbitrary(inputRadix - 1);
+      if (maxCipher < 10) {
+        var numberTest = new RegExp('^-?[0-' + maxCipher + ']*\\' +
+          delimiter + '?[0-' + maxCipher + ']*$');
+      } else {
+        var numberTest = new RegExp('^-?[0-9A-' + maxCipher + ']*\\' +
+          delimiter + '?[0-9A-' + maxCipher + ']*$');
+      }
+      if (!numberTest.test(num)) {
+        throw new TypeError('Wrong input format');
+      }
+      var pointIndex = num.indexOf('.'),
+        power = pointIndex < 0 ? num.length - 1 : pointIndex - 1;
+      if (num[0] === '-') {
+        power--;
+      }
+      for (var i = 0; i < num.length; i++) {
+        if (!isAnyRadixCypher(num[i])) {
+          continue;
+        }
+        result += toDecimal(num[i])
+          * Math.pow(inputRadix, power);
+        power--;
+      }
+      result = '' + (num[0] === '-' ? -result : result);
+      pointIndex = result.indexOf(delimiter);
+      if (pointIndex < 0 && fractionLimit > 0) {
+        pointIndex = result.length + 1;
+        result += '.';
+      } else {
+        pointIndex++;
+      }
+      var fractionLength = result.length - pointIndex;
+      if (pointIndex >= 0 && fractionLength < fractionLimit ) {
+        result += '0'.repeat(fractionLimit - fractionLength);
+      } else if (crop && fractionLimit - fractionLength < 0) {
+        result = result.slice(0, fractionLimit - fractionLength);
+      }
+      return result;
+    } else {
+      if (inputRadix === 10) {
+        num = +num;
+      } else {
+        num = +convert(num, inputRadix, 10);
+      }
+      if (isNaN(num)) {
+        return 0;
+      }
+      var tokens = [];
+      var fraction = Math.abs(num % 1), int = Math.abs(num) - fraction;
+      while (int >= outputRadix) {
+        var newInt = parseInt(int / outputRadix);
+        tokens.push(toArbitrary(int - newInt * outputRadix));
+        int = newInt
+      }
+      if (int !== 0) {
+        tokens.push(toArbitrary(int));
+      }
+      tokens.reverse();
+      if (tokens.length === 0) {
+        tokens.push('0');
+      }
+      var intLength = tokens.length;
+      if (fractionLimit > 0 || fraction > 0) {
+        tokens.push('.');
+      }
+      if (fraction > 0)
+      {
+        while (fraction !== 0 && (tokens[tokens.length - 1] == 0 ||
+          fractionLimit < 0 || !crop ||
+          tokens.length < fractionLimit + intLength)) {
+          int = fraction * outputRadix;
+          fraction = int % 1;
+          tokens.push(toArbitrary(int - fraction));
+        }
+      }
+      var repeatTimes = fractionLimit + intLength - tokens.length + 1;
+      if (fractionLimit > 0 && repeatTimes > 0) {
+        tokens.push('0'.repeat(repeatTimes));
+      }
+      result = (num < 0 ? '-' : '') +
+        (tokens.length ? tokens.join('') : '0');
+      return result;
+    }
+  }
+  
+  convert.convert = convert;
+  convert.toArbitrary = toArbitrary;
+  convert.toDecimal = toDecimal;
+  convert.isAnyRadixNumber = isAnyRadixCypher;
+  Object.defineProperties(convert, {
+    fractionLimit: {
+      get: function() {
+        return fractionLimit;
+      },
+      set: function(number) {
+        if (!isNaN(number = +number) && number >= 0) {
+          fractionLimit = number;
+        }
+      }
+    },
+    delimiter: {
+      get: function() {
+        return delimiter;
+      },
+      set: function(str) {
+        if (str.length === 1 && !isAnyRadixCypher(str)) {
+          delimiter = str;
+        }
+      }
+    },
+    crop: {
+      get: function() {
+        return crop;
+      },
+      set: function(fill) {
+        crop = !!fill;
+      }
+    }
+  });
+  Object.seal(convert);
+  return convert;
+}
+function RadixCalculator(id, delimiter) {
+  if (!window.$) {
+    if (!window.jQuery) {
+      throw new ReferenceError('jQuery is not found. Tested with 3.2.1. ' +
+        'Get at https://code.jquery.com/jquery-3.2.1.min.js or ' +
+        'https://jquery.com/download/');
+    }
+  }
+  if (id[0] !== '#') {
+    id = '#' + id;
+  }
+  try {
+    var container = $(id);
+  } catch (e) {
+    console.error(e);
+    throw new TypeError('Bad container id');
+  }
+  if (!delimiter) {
+    delimiter = '.';
+  } else if (['.', ','].indexOf(delimiter) < 0) {
+    throw new TypeError('Bad delimiter, see previous error');
+  }
+  
+  var tdStyle = {
+    width: '1rem',
+    height: '1rem',
+    'line-height': '100%',
+    padding: 0,
+    border: '1px solid #aaa',
+    'text-align': 'center',
+    'position': 'relative'
+  };
+  var tableStyle = {
+    color: '#000',
+    'font-size': '1rem',
+    'font-family': ['Arial', 'Helvetica', 'sans-serif'],
+    'font-weight': '600',
+    'border-collapse': 'collapse',
+    'table-layout': 'fixed'
+  };
+  var separatorStyle = {
+    position: 'absolute',
+    height: '100%',
+    top: '0.1rem',
+    left: '0.8rem',
+    'font-weight': '900',
+    'font-size': '1.2rem',
+    'z-index': 999,
+    'border-width': 0
+  };
+  var trStyle = {
+  };
+  function buildTable(rows, columns) {
+    container.empty();
+    var table = $('<table>').appendTo(container)
+      .css(tableStyle);
+    var cells = [];
+    for (var i = 0; i < rows; i++) {
+      var row = $('<tr>').appendTo(table)
+        .css(trStyle);
+      cells[i] = [];
+      for (var j = 0; j < columns; j++) {
+        cells[i][j] = $('<td>1</td>').appendTo(row)
+          .css(tdStyle);
+      }
+    }
+    cells[0][1].css('border-color', '#000');
+    return cells;
+  }
+  var cells = buildTable(5,10);
+  
+  // startArray = [row, column], lengthArray = [rowLength, columnLength]
+  function underScore(borderPieces, startArray, lengthArray) {
+    for (var i = startArray[0]; i < startArray[0] + lengthArray[0]; i++) {
+      for (var j = startArray[1]; j < startArray[1] + lengthArray[1]; j++) {
+        if (borderPieces & underScore.enum.TOP) {
+          if (i > 0) {
+            cells[i - 1][j].css('border-bottom-color', '#000');
+          } else {
+            cells[i][j].css('border-top-color', '#000');
+          }
+        }
+        if (borderPieces & underScore.enum.RIGHT) {
+          cells[i][j].css('border-right-color', '#000');
+        }
+        if (borderPieces & underScore.enum.BOTTOM) {
+          cells[i][j].css('border-bottom-color', '#000');
+        }
+        if (borderPieces & underScore.enum.LEFT) {
+          if (j > 0) {
+            cells[i][j - 1].css('border-right-color', '#000');
+          } else {
+            cells[i][j].css('border-left-color', '#000');
+          }
+        }
+      }
+    }
+  }
+  underScore.enum = {
+    TOP: 2,
+    RIGHT: 4,
+    BOTTOM: 8,
+    LEFT: 16
+  };
+}
+RadixCalculator('output');
+
+angular.module('radix-converter', [])
   .controller('mainController', function($scope) {
     $scope.dec = 0;
     $scope.hex = 0;
@@ -24,183 +309,6 @@ angular.module('radix-converter', ["ui.bootstrap"])
     //   $scope.oct = convert(newValue, 2, 8);
     //   $scope.dec = convert(newValue, 2, 10);
     // });
-    function RadixConverter() {
-      var convertTokens = {};
-      function toDecimal(token) {
-        if (!convertTokens[token]) {
-          var num = parseInt(token);
-          if (num >= 0 && num < 10) {
-            convertTokens[token] = num;
-          } else {
-            token += '';
-            var code = token.charCodeAt(0);
-            if (convertTokens[token] < 65 ||
-              convertTokens[token] > 90) {
-              throw new TypeError('Not an uppercase letter nor number');
-            }
-            convertTokens[token] = code - 55;
-          }
-        }
-        return convertTokens[token];
-      }
-      function toArbitrary(num) {
-        if (typeof num !== 'number' || num < 0 || num > 36) {
-          throw new TypeError('Number must be between 2 and 36');
-        }
-        num = parseInt(num);
-        if (!convertTokens[num]) {
-          if (num < 10) {
-            convertTokens[num] = num;
-          } else {
-            convertTokens[num] = String.fromCharCode(55 + num);
-          }
-        }
-        return convertTokens[num];
-      }
-      var alphaNumRegex = /^[A-Z0-9]+$/;
-      function isAnyRadixCypher(str) {
-        return alphaNumRegex.test(str);
-      }
-  
-      var fractionLimit = -1;
-      var crop = false;
-      var delimiter = '.';
-      function convert(num, inputRadix, outputRadix)
-      {
-        inputRadix = parseInt(inputRadix);
-        outputRadix = parseInt(outputRadix);
-        if (isNaN(outputRadix) || isNaN(inputRadix) || inputRadix < 2
-          || inputRadix > 36 || outputRadix < 2 || outputRadix > 36) {
-          throw new TypeError('Radix must been between 2 and 36');
-        } else if (inputRadix === outputRadix) {
-          throw new TypeError('Radixes are equal');
-        }
-    
-        var result = 0;
-        if (outputRadix === 10) {
-          num = (num + '').toUpperCase();
-          var maxCipher = toArbitrary(inputRadix - 1);
-          if (maxCipher < 10) {
-            var numberTest = new RegExp('^-?[0-' + maxCipher + ']*\\' +
-              delimiter + '?[0-' + maxCipher + ']*$');
-          } else {
-            var numberTest = new RegExp('^-?[0-9A-' + maxCipher + ']*\\' +
-              delimiter + '?[0-9A-' + maxCipher + ']*$');
-          }
-          if (!numberTest.test(num)) {
-            throw new TypeError('Wrong input format');
-          }
-          var pointIndex = num.indexOf('.'),
-            power = pointIndex < 0 ? num.length - 1 : pointIndex - 1;
-          if (num[0] === '-') {
-            power--;
-          }
-          for (var i = 0; i < num.length; i++) {
-            if (!isAnyRadixCypher(num[i])) {
-              continue;
-            }
-            result += toDecimal(num[i])
-              * Math.pow(inputRadix, power);
-            power--;
-          }
-          result = '' + (num[0] === '-' ? -result : result);
-          pointIndex = result.indexOf(delimiter);
-          if (pointIndex < 0 && fractionLimit > 0) {
-            pointIndex = result.length + 1;
-            result += '.';
-          } else {
-            pointIndex++;
-          }
-          var fractionLength = result.length - pointIndex;
-          if (pointIndex >= 0 && fractionLength < fractionLimit ) {
-            result += '0'.repeat(fractionLimit - fractionLength);
-          } else if (crop && fractionLimit - fractionLength < 0) {
-            result = result.slice(0, fractionLimit - fractionLength);
-          }
-          return result;
-        } else {
-          if (inputRadix === 10) {
-            num = +num;
-          } else {
-            num = +convert(num, inputRadix, 10);
-          }
-          if (isNaN(num)) {
-            return 0;
-          }
-          var tokens = [];
-          var fraction = Math.abs(num % 1), int = Math.abs(num) - fraction;
-          while (int >= outputRadix) {
-            var newInt = parseInt(int / outputRadix);
-            tokens.push(toArbitrary(int - newInt * outputRadix));
-            int = newInt
-          }
-          if (int !== 0) {
-            tokens.push(toArbitrary(int));
-          }
-          tokens.reverse();
-          if (tokens.length === 0) {
-            tokens.push('0');
-          }
-          var intLength = tokens.length;
-          if (fractionLimit > 0 || fraction > 0) {
-            tokens.push('.');
-          }
-          if (fraction > 0)
-          {
-            while (fraction !== 0 && (tokens[tokens.length - 1] == 0 ||
-              fractionLimit < 0 || !crop ||
-              tokens.length < fractionLimit + intLength)) {
-              int = fraction * outputRadix;
-              fraction = int % 1;
-              tokens.push(toArbitrary(int - fraction));
-            }
-          }
-          var repeatTimes = fractionLimit + intLength - tokens.length + 1;
-          if (fractionLimit > 0 && repeatTimes > 0) {
-            tokens.push('0'.repeat(repeatTimes));
-          }
-          result = (num < 0 ? '-' : '') +
-            (tokens.length ? tokens.join('') : '0');
-          return result;
-        }
-      }
-      
-      convert.convert = convert;
-      convert.toArbitrary = toArbitrary;
-      convert.toDecimal = toDecimal;
-      convert.isAnyRadixNumber = isAnyRadixCypher;
-      Object.defineProperties(convert, {
-          fractionLimit: {
-            get: function() {
-              return fractionLimit;
-            },
-            set: function(number) {
-              if (!isNaN(number = +number) && number >= 0) {
-                fractionLimit = number;
-              }
-            }
-          },
-          delimiter: {
-            get: function() {
-              return delimiter;
-            },
-            set: function(str) {
-              if (str.length === 1 && !isAnyRadixCypher(str)) {
-                delimiter = str;
-              }
-            }
-          },
-          crop: {
-            get: function() {
-              return crop;
-            },
-            set: function(fill) {
-              crop = !!fill;
-            }
-          }
-        });
-      Object.seal(convert);
-      return convert;
-    }
+
     var convert = new RadixConverter;
   });
